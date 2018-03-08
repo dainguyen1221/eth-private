98eb83668d76a85731f8e9a4d26bb1bd94fb8069
Jaynti Kanani <jdkanani@gmail.com>
2018-03-08 15:33:15 +0530

    geth PoA

--- geth/clean.sh
+++ geth/clean.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env sh
-rm -rf data/*
-mkdir -p data/keystore
-cp keystore/* data/keystore/
-geth --datadir "$PWD/data" init ./genesis.json
+
+pkill -F $PWD/bootnode.pid
+rm bootnode.log bootnode.pid
+
+pkill -F $PWD/data1/node.pid
+rm -rf data1/*
+mkdir -p data1/keystore
+
+pkill -F $PWD/data2/node.pid
+rm -rf data2/*
+mkdir -p data2/keystore
+
+cp keystore/UTC--2018-02-19T10-26-21.638675000Z--9fb29aac15b9a4b7f17c3385939b007540f4d791 data1/keystore/
+cp keystore/UTC--2018-02-18T14-45-31.445018000Z--acf8eccdca12a0eb6ae4fb1431e26c44e66decdb data2/keystore/
+
+geth --datadir "$PWD/data1" init ./genesis.json
+geth --datadir "$PWD/data2" init ./genesis.json
--- geth/start.sh
+++ geth/start.sh
@@ -1,2 +1,41 @@
 #!/usr/bin/env sh
-geth --datadir "$PWD/data" --targetgaslimit '9000000000000' --rpc --rpcport 8546 --rpccorsdomain "*" --wsorigins "*" --unlock "0x9fb29aac15b9a4b7f17c3385939b007540f4d791,0xacf8eccdca12a0eb6ae4fb1431e26c44e66decdb" --password "$PWD/node.password" --networkid 13 --mine
+
+nohup bootnode -nodekey bootnode.key > bootnode.log 2>&1 &
+echo $! > $PWD/bootnode.pid
+echo "Bootnode stared."
+
+sleep 5
+
+nohup geth \
+  --bootnodes enode://744ae75e44fb51f95a92a5ca29f8f8273b2501e3025157e9fca30b01a4baf2c01912dc9269a24b6c14642551d7dc50479244a7e5cd35e09446850730967520f3@127.0.0.1:30301 \
+  --datadir "$PWD/data1"  \
+  --syncmode 'full' \
+  --port 30303  \
+  # --rpcapi 'personal,db,eth,net,web3,txpool,miner' \
+  --rpc --rpcport 8545  \
+  --wsport 8546 --rpccorsdomain "*"  \
+  --wsorigins "*"  \
+  --gasprice "0" \
+  --unlock "0x9fb29aac15b9a4b7f17c3385939b007540f4d791"  \
+  --password "$PWD/node.password"  \
+  --networkid 13 --mine > $PWD/data1/node.log 2>&1 &
+echo $! > $PWD/data1/node.pid
+echo "Node 1 started. Check data1/node.log for more logs"
+
+sleep 10
+
+nohup geth \
+  --bootnodes enode://744ae75e44fb51f95a92a5ca29f8f8273b2501e3025157e9fca30b01a4baf2c01912dc9269a24b6c14642551d7dc50479244a7e5cd35e09446850730967520f3@127.0.0.1:30301 \
+  --datadir "$PWD/data2" \
+  --syncmode 'full' \
+  --port 40303 \
+  # --rpcapi 'personal,db,eth,net,web3,txpool,miner' \
+  --rpc --rpcport 9545 \
+  --wsport 9546 --rpccorsdomain "*" \
+  --wsorigins "*" \
+  --gasprice "0" \
+  --unlock "0xacf8eccdca12a0eb6ae4fb1431e26c44e66decdb" \
+  --password "$PWD/node.password" \
+  --networkid 13 --mine > $PWD/data2/node.log 2>&1 &
+echo $! > $PWD/data2/node.pid
+echo "Node 2 started. Check data2/node.log for more logs"
